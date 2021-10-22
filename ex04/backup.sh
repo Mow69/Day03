9e982d6a80a6b786d913e471653c8d066babb6f9
Mouaz Saadaoui <mouaz@MacBook-Air-de-Mouaz.local>
2021-10-22 17:52:55 +0200

    add Wahid corrections : ex00, ex02, ex03, ex04

--- ex04/backup.sh
+++ ex04/backup.sh
@@ -1,1 +1,1 @@
-30 7 * * * tar cvf ~/Documents/backup/"backup_$(date +%a_%d_%b_%Y_%H_%M_%S_%Z).tar.gz" ~/Pictures
+30 7 * * * mkdir -p ~/Documents/backup ; tar cvf ~/Documents/backup/"backup_$(date | sed -e 's/[,: ]/__/g' -e 's/__/_/g')".tar.gz Pictures
